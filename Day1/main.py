033a06ae01a144bf3578fd5a3681fd59d91a0d10
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2024-01-04 14:20:13 +0700

    Python bootcam day 1

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What is your city you grew up: ")
+pet = input("What is your pet name: ")
+print("Hello you may join a band with name "+ city + "-"+pet)
