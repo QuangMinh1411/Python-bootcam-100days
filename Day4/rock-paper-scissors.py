5d66bda5dae7591014b6422621a54ac41429c60e
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2024-01-19 00:43:31 +0700

    List, random

--- Day4/rock-paper-scissors.py
+++ Day4/rock-paper-scissors.py
@@ -30,22 +30,24 @@ scissors = '''
 game_images = [rock,paper,scissors]
 
 human_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-computer_choice = random.randint(0,len(game_images)-1)
-print(game_images[human_choice])
-    
-print(f"Computer chose:\n {game_images[computer_choice]}")
-if human_choice>=3 and human_choice<=0:
+if human_choice>=3 or human_choice<=0:
     print("You typed invalid number, you lose")
-elif human_choice==0 and computer_choice==2:
-    print("You win")
-elif computer_choice==0 and human_choice==2:
-    print("You lose")
-elif computer_choice>human_choice:
-    print("You lose")
-elif computer_choice<human_choice:
-    print("You win")
 else:
-    print("Deuce")
+    print(game_images[human_choice])
+    computer_choice = random.randint(0,len(game_images)-1)
+    
+    print(f"Computer chose:\n {game_images[computer_choice]}")
+
+    if human_choice==0 and computer_choice==2:
+        print("You win")
+    elif computer_choice==0 and human_choice==2:
+        print("You lose")
+    elif computer_choice>human_choice:
+        print("You lose")
+    elif computer_choice<human_choice:
+        print("You win")
+    else:
+        print("Deuce")
     
 
 
